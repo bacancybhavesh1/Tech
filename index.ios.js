faf106b7ed39d71e81bcaaf12d1586f529e51f48
admin <admin@bhaveshs-Mac-mini.local>
2017-03-01 16:56:54 +0530

    title changes

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,5 @@
 /**
- * Sample React Native App
+ * Tech App
  * https://github.com/facebook/react-native
  * @flow
  */
@@ -12,42 +12,6 @@ import {
   View
 } from 'react-native';
 
-export default class Tech extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import App from './src/app';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('Tech', () => Tech);
+AppRegistry.registerComponent('Tech', () => App);
